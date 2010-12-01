d306cd6f9fdf7e3332ee4b6e34653a7904297fa9
Anne M. Archibald <peridot.faceted@gmail.com>
2010-12-01 01:17:11 -0500

    Removed pointless vestigial loop

--- initialise.C
+++ initialise.C
@@ -151,9 +151,6 @@ void initialiseOne (pulsar *psr, int noWarnings, int fullSetup)
     }
   
   allocateMemory(psr,0);
-  for (j=0;j<MAX_PARAMS;j++) {
-
-  }
   /*  psr->param[param_track].paramSet[0]=1;
   psr->param[param_track].val[0]=0.0;
   psr->param[param_track].prefit[0]=0.0;*/
